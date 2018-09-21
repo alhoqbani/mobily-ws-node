3a71fb1a200cb4c69b85bbb2535fd27cef24028a
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-09-20 19:04:47 -0700

    Switch to tsc to transpile typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {MobilyWs} from "./MobilyWs";
 
-export function mobilyWs(username: string, password: number): MobilyWs {
+export = function mobilyWs(username: string, password: number): MobilyWs {
     return new MobilyWs(username, password);
 }

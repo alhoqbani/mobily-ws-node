8aaabc5baea42f1827abbd9aa041b0d6e87aff6b
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-09-20 18:57:21 -0700

    add mobilyWs function

---
+++ src/MobilyWs.ts
@@ -0,0 +1,10 @@
+export class MobilyWs {
+
+    private _username: string;
+    private _password: number;
+
+    constructor(username: string, password: number) {
+        this._username = username;
+        this._password = password;
+    }
+}
